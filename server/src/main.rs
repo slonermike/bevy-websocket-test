0a589e7de0482ba61652aa58b620dceef531b8c0
Mike Slone <michaelgareth@gmail.com>
2026-04-02 21:30:08 -0400

    Move ball logic to plugin.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
+mod plugins;
+
 use bevy::prelude::*;
+use plugins::ball::BallPlugin;
 
 fn main() {
     App::new()
@@ -9,39 +12,11 @@ fn main() {
             }),
             ..default()
         }))
-        .add_message::<SpawnBallMessage>()
         .add_systems(Startup, setup_camera)
-        .add_systems(Update, spawn_ball)
-        .add_systems(Startup, send_test_spawn)
+        .add_plugins(BallPlugin)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
-}
-
-#[derive(Message)]
-struct SpawnBallMessage {
-    position: Vec2,
-}
-
-fn spawn_ball(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut messages: MessageReader<SpawnBallMessage>,
-) {
-    for message in messages.read() {
-        commands.spawn((
-            Mesh2d(meshes.add(Circle::new(25.0))),
-            MeshMaterial2d(materials.add(Color::srgb(1.0, 0.0, 0.0))),
-            Transform::from_translation(message.position.extend(0.0)),
-        ));
-    }
-}
-
-fn send_test_spawn(mut writer: MessageWriter<SpawnBallMessage>) {
-    writer.write(SpawnBallMessage {
-        position: Vec2::new(0.0, 0.0),
-    });
 }
---
+++ server/src/plugins/ball.rs
@@ -0,0 +1,58 @@
+use bevy::prelude::*;
+use rand::RngExt;
+
+pub struct BallPlugin;
+
+#[derive(Resource)]
+struct SpawnTimer(Timer);
+
+#[derive(Message)]
+struct SpawnBallMessage {
+    position: Vec2,
+}
+
+fn spawn_ball_handler(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut messages: MessageReader<SpawnBallMessage>,
+) {
+    for message in messages.read() {
+        commands.spawn((
+            Mesh2d(meshes.add(Circle::new(25.0))),
+            MeshMaterial2d(materials.add(Color::srgb(1.0, 0.0, 0.0))),
+            Transform::from_translation(message.position.extend(0.0)),
+        ));
+    }
+}
+
+fn spawn_timer_system(
+    mut timer: ResMut<SpawnTimer>,
+    time: Res<Time>,
+    mut writer: MessageWriter<SpawnBallMessage>,
+) {
+    timer.0.tick(time.delta());
+
+    if timer.0.just_finished() {
+        let mut rng = rand::rng();
+
+        let rand_x = rng.random_range(-150.0..150.0);
+        let rand_y = rng.random_range(-150.0..150.0);
+        writer.write(SpawnBallMessage {
+            position: Vec2::new(rand_x, rand_y),
+        });
+
+        let next_duration = rng.random_range(0.5..1.5);
+        timer
+            .0
+            .set_duration(std::time::Duration::from_secs_f32(next_duration));
+    }
+}
+
+impl Plugin for BallPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_message::<SpawnBallMessage>()
+            .insert_resource(SpawnTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
+            .add_systems(Update, (spawn_timer_system, spawn_ball_handler));
+    }
+}
