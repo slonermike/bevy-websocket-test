cf322f29b35c83aaa7c04b9ffbcacaf8a8c3f3fc
Mike Slone <michaelgareth@gmail.com>
2026-04-02 21:52:36 -0400

    Add boundaries.

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, (setup_camera, setup_walls))
         .add_plugins(BallPlugin)
         .add_plugins(PhysicsPlugins::default())
         .insert_resource(Gravity(Vec2::new(0.0, -500.0)))
@@ -22,4 +22,28 @@ fn main() {
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+fn setup_walls(mut commands: Commands, windows: Query<&Window>) {
+    let window = windows.single().unwrap();
+    let width = window.width();
+    let height = window.height();
+
+    commands.spawn((
+        RigidBody::Static,
+        Collider::rectangle(width, 20.0),
+        Transform::from_xyz(0.0, height * -0.5, 0.0),
+    ));
+
+    commands.spawn((
+        RigidBody::Static,
+        Collider::rectangle(20.0, height),
+        Transform::from_xyz(width * -0.5, 0.0, 0.0),
+    ));
+
+    commands.spawn((
+        RigidBody::Static,
+        Collider::rectangle(20.0, height),
+        Transform::from_xyz(width * 0.5, 0.0, 0.0),
+    ));
 }
